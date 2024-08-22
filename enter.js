9411750c0359461426fc33e6f2f57773b8d35ef6
ChuangB48 <kecianchuang@gmail.com>
2024-08-22 23:11:21 +0800

    hi

--- enter.js
+++ enter.js
@@ -2,6 +2,7 @@ function submit(){
     const p=document.getElementById("name");
     for(let a=0;a<ap.length;a++){
         if(ap[a].name==p.value){
+            document.cookie="name="+ap[a].name+"; max-age=43200";
             location.href="edu-lobby.html";
         }
     }
