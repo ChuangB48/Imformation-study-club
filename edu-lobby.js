9e6181c10cc0842f7df82d676d665c9fc01380c4
ChuangB48 <kecianchuang@gmail.com>
2024-08-22 23:28:18 +0800

    add cookie

--- edu-lobby.js
+++ edu-lobby.js
@@ -1,3 +1,13 @@
+let cookies=document.cookie.split(";");
+for(let a=0;a<cookies.length;a++){
+    cookies[a].trim();
+}
+for(let a=0;a<cookies.length;a++){
+    let detel=cookies[a].split("=");
+    if(detel[0]=="name"){
+        document.getElementById("log").innerHTML+="<span style='font-size:90px;font-family:DFKai-sb;'>"+detel[1]+"</span>";
+    }
+}
 function mass(){
     location.href="massage.html";
 }
