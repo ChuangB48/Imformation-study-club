b2e8d61484ce85b14b95952f9e1fbff2ecb47830
ChuangB48 <kecianchuang@gmail.com>
2024-08-22 16:48:35 +0800

    hi

--- book.js
+++ book.js
@@ -1,3 +1,3 @@
 for(let a=0;a<ab.length;a++){
-    document.getElementById("board").innerHTML+="<div>"+ab[a].title+"</div><div>"+ab[a].text+"</div>";
+    document.getElementById("board").innerHTML+="<div><span>"+ab[a].title+"</span></div><div>"+ab[a].text+"</div>";
 }
