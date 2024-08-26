b74305c2f9edb69fcbcc6219fa1af11fe508a58b
ChuangB48 <kecianchuang@gmail.com>
2024-08-27 00:44:24 +0800

    add more person

--- All Person.js
+++ All Person.js
@@ -1,5 +1,8 @@
 let ap=[
-    {num:"1121131",name:"曾子恆",score:0},
+    {num:"1121034",name:"鍾王誠",score:0},
     {num:"1121126",name:"莊可謙",score:0},
-    {num:"1121130",name:"陳葆謙",score:0}
+    {num:"1121130",name:"陳葆謙",score:0},
+    {num:"1121131",name:"曾子恆",score:0},
+    {num:"1121527",name:"陳柏舜",score:0},
+    {num:"1121805",name:"吳昀耘",score:0}
 ];
