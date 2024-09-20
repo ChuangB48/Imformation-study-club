7d661019a454d4c42eeda9b0b55c166e65dea063
ChuangB48 <kecianchuang@gmail.com>
2024-09-20 23:53:03 +0800

    add question

---
+++ ques.js
@@ -0,0 +1,4 @@
+let q=[
+    {num:"Hello world",locate:"https://zerojudge.tw/ShowProblem?problemid=a001"},
+    {num:"簡易加法",locate:"https://zerojudge.tw/ShowProblem?problemid=a002"}
+];
--- solve.js
+++ solve.js
@@ -1,3 +1,6 @@
 function ba(){
     location.href="edu-lobby.html";
 }
+for(let a=0;a<q.length;a++){
+    document.getElementById("board").innerHTML+="<a href="+q[a].locate+">&bull;"+q[a].num+"</a><br>";
+}
