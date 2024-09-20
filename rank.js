34ad2f99bed63d9476fbb750268b24114c073466
ChuangB48 <kecianchuang@gmail.com>
2024-09-20 23:58:36 +0800

    yaa

--- rank.js
+++ rank.js
@@ -9,7 +9,7 @@ allvalue.sort(function(A,B){return B-A;});
 for(a=0;a<allvalue.length;a++){
     for(let b in allscore){
         if(allvalue[a]==allscore[b]){
-            document.getElementById("board").innerHTML+="<div style='width:1436px;border:1px rgb(250,130,130) solid'>"+b+"  "+allscore[b]+"</div>";
+            document.getElementById("board").innerHTML+="<div style='width:1400px;border:1px rgb(250,130,130) solid'>"+b+"  "+allscore[b]+"</div>";
             allscore[b]=-1;
         }
     }
