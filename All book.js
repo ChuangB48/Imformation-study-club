781f2de549a4f4b3b1b8e3f3244fe35b9cc126c2
ChuangB48 <kecianchuang@gmail.com>
2024-09-26 23:04:20 +0800

    change book

---
+++ All book.js
@@ -0,0 +1,4 @@
+let b=[
+    {num:"C++基礎語法",locate:"https://hackmd.io/@1xt-uZ5mQhqj71SggfJKxw/SkB-hobs0"}
+];
+//{num:"",locate:""}
--- book.js
+++ book.js
@@ -1,3 +1,6 @@
 function ba(){
     location.href="edu-lobby.html";
 }
+for(let a=0;a<b.length;a++){
+    document.getElementById("board").innerHTML+="<a target='_blank' href="+b[a].locate+">&bull;"+b[a].num+"</a><br>";
+}
