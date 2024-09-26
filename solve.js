a02022d707a817d75bbcb0b06f7c3f096f8ab08d
ChuangB48 <kecianchuang@gmail.com>
2024-09-26 22:54:59 +0800

    chang target

--- solve.js
+++ solve.js
@@ -2,5 +2,5 @@ function ba(){
     location.href="edu-lobby.html";
 }
 for(let a=0;a<q.length;a++){
-    document.getElementById("board").innerHTML+="<a href="+q[a].locate+">&bull;"+q[a].num+"</a><br>";
+    document.getElementById("board").innerHTML+="<a target='_blank' href="+q[a].locate+">&bull;"+q[a].num+"</a><br>";
 }
