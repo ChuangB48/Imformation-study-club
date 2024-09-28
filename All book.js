509d2696ccb38cbe36ff103ebe771f37cbd6b0a6
ChuangB48 <kecianchuang@gmail.com>
2024-09-28 11:19:45 +0800

    change const

--- All book.js
+++ All book.js
@@ -1,4 +1,4 @@
-let b=[
+const b=[
     {num:"C++基礎語法",locate:"https://hackmd.io/@1xt-uZ5mQhqj71SggfJKxw/SkB-hobs0"}
 ];
 //{num:"",locate:""}
--- All ques.js
+++ All ques.js
@@ -1,4 +1,4 @@
-let q=[
+const q=[
     {num:"Hello world",locate:"https://zerojudge.tw/ShowProblem?problemid=a001"},
     {num:"簡易加法",locate:"https://zerojudge.tw/ShowProblem?problemid=a002"}
 ];
