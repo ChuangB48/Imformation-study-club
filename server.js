f9de91ee4e1aea51610dd3c823d14ab4a3e09f49
ChuangB48 <kecianchuang@gmail.com>
2024-10-03 09:20:32 +0800

    add com

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const express=require("express");
 const WebSocket=require("ws").Server;
 const server=express().listen(9172,function(){});
 const wss=new WebSocket({server});
+let a=0;
 wss.on("connection",function(ws){
+    console.log("computer number now: ",a);
+    a++;
+    ws.on("close",function(){
+        a--;
+        console.log("computer number now: ",a);
+    });
     ws.on("message",function(data){
         data=data.toString();
         ws.send(data);
