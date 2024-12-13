0240aac981e3770245e7b9f8e50484896b5d9419
Henry960917 <148993233+Henry960917@users.noreply.github.com>
2024-12-13 15:32:09 +0800

    Update All Person.js
    score 12/13

--- All Person.js
+++ All Person.js
@@ -1,18 +1,18 @@
 const ap=[
-    {num:"1130201",name:"吳紹慈",score:0,group:10},//ㄨˊ ㄕㄠˋ ㄘˊ
-    {num:"1130227",name:"傅紳豪",score:0,group:2},//ㄈㄨˋ ㄕㄣ ㄏㄠˊ
+    {num:"1130201",name:"吳紹慈",score:80,group:10},//ㄨˊ ㄕㄠˋ ㄘˊ
+    {num:"1130227",name:"傅紳豪",score:20,group:2},//ㄈㄨˋ ㄕㄣ ㄏㄠˊ
     {num:"1130229",name:"劉芫睿",score:0,group:10},//ㄌㄧㄡˊ ㄩㄢˊ ㄖㄨㄟˋ
-    {num:"1130511",name:"游倚楓",score:0,group:10},//ㄧㄡˊ ㄧˇ ㄈㄥ
-    {num:"1130524",name:"康鈞惟",score:0,group:9},//ㄎㄤ ㄐㄩㄣ ㄨㄟˊ
-    {num:"1130712",name:"賴知琪",score:0,group:10},//ㄌㄞˋ ㄓ ㄑㄧˊ
-    {num:"1130902",name:"李羽舒",score:0,group:8},//ㄌㄧˇ ㄩˇ ㄕㄨ
-    {num:"1131020",name:"林晉詣",score:0,group:7},//ㄌㄧㄣˊ ㄐㄧㄣˋ ㄧˋ
-    {num:"1131032",name:"廖偉恩",score:0,group:5},//ㄌㄧㄠˋ ㄨㄟˇ ㄣ
-    {num:"1131118",name:"林子宸",score:0,group:5},//ㄌㄧㄣˊ ㄗˇ ㄔㄣˊ
+    {num:"1130511",name:"游倚楓",score:100,group:10},//ㄧㄡˊ ㄧˇ ㄈㄥ
+    {num:"1130524",name:"康鈞惟",score:40,group:9},//ㄎㄤ ㄐㄩㄣ ㄨㄟˊ
+    {num:"1130712",name:"賴知琪",score:20,group:10},//ㄌㄞˋ ㄓ ㄑㄧˊ
+    {num:"1130902",name:"李羽舒",score:100,group:8},//ㄌㄧˇ ㄩˇ ㄕㄨ
+    {num:"1131020",name:"林晉詣",score:60,group:7},//ㄌㄧㄣˊ ㄐㄧㄣˋ ㄧˋ
+    {num:"1131032",name:"廖偉恩",score:20,group:5},//ㄌㄧㄠˋ ㄨㄟˇ ㄣ
+    {num:"1131118",name:"林子宸",score:20,group:5},//ㄌㄧㄣˊ ㄗˇ ㄔㄣˊ
     {num:"1131315",name:"古秉軒",score:0,group:8},//ㄍㄨˇ ㄅㄧㄥˇ ㄒㄩㄢ
-    {num:"1131524",name:"高睿濬",score:0,group:5},//ㄍㄠ ㄖㄨㄟˋ ㄐㄩㄣˋ
+    {num:"1131524",name:"高睿濬",score:20,group:5},//ㄍㄠ ㄖㄨㄟˋ ㄐㄩㄣˋ
     {num:"1131602",name:"洪慧幀",score:0,group:6},//ㄏㄨㄥˊ ㄏㄨㄟˋ ㄓㄥˋ
-    {num:"1131609",name:"游牧仁",score:0,group:0},//ㄧㄡˊ ㄇㄨˋ ㄖㄣˊ
+    {num:"1131609",name:"游牧仁",score:20,group:0},//ㄧㄡˊ ㄇㄨˋ ㄖㄣˊ
     {num:"1131817",name:"林子錡",score:0,group:0},//ㄌㄧㄣˊ ㄗˇ ㄑㄧˊ
     {num:"1120306",name:"李嘉媛",score:0,group:3},//ㄌㄧˇ ㄐㄧㄚ ㄩㄢˊ
     {num:"1121523",name:"張哲瑋",score:0,group:9},//ㄓㄤ ㄓㄜˊ ㄨㄟˇ
